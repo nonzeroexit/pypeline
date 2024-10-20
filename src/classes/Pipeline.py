29d1d0ba3298d3cab3704b68a9e9c6cd678b2e79
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-20 10:10:20 -0300

    implements Pipeline class

--- src/classes/Pipeline.py
+++ src/classes/Pipeline.py
@@ -1,6 +1,7 @@
 class Pipeline:
     def __init__(self, steps):
         self.steps = steps
+        self.params = {}
         self.step_index = 0
 
     def next_step(self):
@@ -9,3 +10,6 @@ class Pipeline:
     def previous_step(self):
         if self.step_index > 0:
             self.step_index -= 1
+
+    def __len__(self):
+        return len(self.steps)
--- src/pypeline.py
+++ src/pypeline.py
@@ -1,41 +1,42 @@
 import os
 import sys
+from classes.Pipeline import Pipeline
 from modules import utils
 from modules import log
 
 def run_pipeline(pipeline_filename):
     steps = utils.get_steps(pipeline_filename)
-    used_params = {}
-    for step in steps:
-        log.add(f'## Step {step.name}', True)
-        while True:
-            step.print_info(used_params)
-            option = utils.ask_what_to_do(step)
-            match option:
-                case 'run':
-                    step.get_params(used_params)
-                    step.run()
-                    step.write_to_log()
-                    break
-                case 'modify_cmd':
-                    step.change_command()
-                    continue
-                case 'skip':
-                    log.add('* **Skipped step**')
-                    break
-                case 'exit':
-                    log.add('* **Pipeline ended**')
-                    sys.exit(0)
-        utils.ask_add_comment()
-        used_params = {**used_params, **step.params}
+    pipeline = Pipeline(steps)
+    while pipeline.step_index < len(pipeline):
+        step = steps[pipeline.step_index]
+        step.print_info(pipeline.params)
+        option = utils.ask_what_to_do(step)
+        match option:
+            case 'run':
+                log.add(f'## Step {step.name}', True)
+                step.get_params(pipeline.params)
+                step.run()
+                step.write_to_log()
+                utils.ask_add_comment()
+                pipeline.params = {**pipeline.params, **step.params}
+                pipeline.next_step()
+            case 'modify_cmd':
+                step.change_command()
+                continue
+            case 'skip':
+                pipeline.next_step()
+                continue
+            case 'exit':
+                log.add('**Pipeline ended**')
+                sys.exit(0)
 
 def main():
     pipeline_filename = utils.get_pipeline_path()
     log.start(pipeline_filename)
-    log.add(f'# Starting pipeline {pipeline_filename.replace(".csv", "")}', True)
+    log.add('# Starting pipeline', True)
     files_at_start = [xfile for xfile in os.listdir(os.curdir) if os.path.isfile(xfile)]
     run_pipeline(pipeline_filename)
-    log.add(f'# Pipeline finished successfully {pipeline_filename.replace(".csv", "")}', True)
+    log.add('# Pipeline finished successfully', True)
     utils.ask_files_to_delete(files_at_start)
 
 if __name__ == '__main__':
