fb97567ee49aed9ad2714abef711fe3d4812f96e
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-21 10:35:22 -0300

    nothing important

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -7,7 +7,7 @@ from modules.make_pipeline import main as make_pipeline
 def get_pipeline_path():
     if len(sys.argv) < 3 or sys.argv[1] not in ['run', 'make']:
         sys.exit(f'usage:\n  pypeline run/make pipeline_file/pipeline_name')
-    option = sys.argv[1] # run or make
+    option = sys.argv[1]
     if option == 'run':
         pipeline_file_path = sys.argv[2]
         if not os.path.isfile(pipeline_file_path):
@@ -23,7 +23,7 @@ def get_pipeline_path():
 def ask_files_to_delete(files_at_start):
     created_files = [xfile for xfile in os.listdir(os.curdir) if os.path.isfile(xfile) and xfile not in files_at_start]
     if created_files:
-        print('Delete created files...')
+        print('Delete created files?')
         for xfile in created_files:
             if input(f'Delete {xfile}?[n]: ').lower() == 'y':
                 os.remove(xfile)
