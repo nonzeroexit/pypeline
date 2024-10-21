ad0a5d42b197f94edb74bef75401948504d25b5c
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-21 09:28:49 -0300

    adds make_pipeline module

--- src/modules/make_pipeline.py
+++ src/modules/make_pipeline.py
@@ -1,2 +1,11 @@
-def main():
-    pass
+def main(pipeline_file_path):
+    lines = []
+    print('Making new pipeline.\nUse % before a param so it can be changed when running the pipeline.')
+    add_new_step = True
+    while add_new_step:
+        step_name = input('Step name: ')
+        step_command = input('Command: ')
+        lines.append(f'{step_name},{step_command}')
+        add_new_step = input('Add new step?[y]: ') != 'n'
+    with open(pipeline_file_path, 'w', encoding='utf-8') as fhandle:
+        fhandle.write(('\n').join(lines))
