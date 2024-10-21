2d47ad5255dc0eb42f167eb42f1d0d7b8914c8f7
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-21 10:21:48 -0300

    adds Step.used_command attribute and logs original command too

--- src/classes/Step.py
+++ src/classes/Step.py
@@ -18,24 +18,26 @@ class Step:
         print('Set params...')
         new_params = {param: input(f'{param}: ') for param in self.command.split() if param.startswith('%') and param not in used_params}
         self.params = {**used_params, **new_params}
+        self.used_command = self.command
         for param, value in self.params.items():
-            self.command = self.command.replace(param, value)
+            self.used_command = self.used_command.replace(param, value)
 
     def run(self):
         files_before = [xfile for xfile in os.listdir(os.curdir) if os.path.isfile(xfile)]
-        exit_code = os.system(self.command)
+        exit_code = os.system(self.used_command)
         self.created_files = [xfile for xfile in os.listdir(os.curdir) if xfile not in files_before and os.path.isfile(xfile)]
         return exit_code
 
     def write_to_log(self, exit_code):
-        log.add(f'* Used command: {self.command} {"**COMMAND WAS CHANGED**" if self.command_was_changed else ""}')
+        log.add(f'* Command: {self.command} {"**COMMAND WAS CHANGED**" if self.command_was_changed else ""}')
+        log.add(f'* Used command: {self.used_command}')
+        if self.params:
+            log.add(f'* Used params: {(", ").join([(" = ").join((param, value)) for param,value in self.params.items()])}')
         if exit_code != 0:
             log.add(f'**NON-ZERO EXIT STATUS** Exit code: {exit_code}')
         else:
             if self.created_files: # if non-zero exit, created files will be deleted
                 log.add(f'* Created files: {(" ").join(self.created_files)}')
-            if self.params:
-                log.add(f'* Used params: {(", ").join([(" = ").join((param, value)) for param,value in self.params.items()])}')
 
     def clean_to_retry(self):
         self.params = {}
