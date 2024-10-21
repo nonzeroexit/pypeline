f48c137336c7da2d123e674d9380e81a25b30ce7
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-21 13:17:53 -0300

    removes useless .replace()

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -4,7 +4,7 @@ from modules.make_pipeline import main as make_pipeline
 
 def get_pipeline_path():
     if len(sys.argv) < 3 or sys.argv[1] not in ['run', 'make']:
-        sys.exit(f'usage:\n  pypeline run/make pipeline_file/pipeline_name')
+        sys.exit(f'usage:\n  pypeline run/make pipeline_file')
     option = sys.argv[1]
     if option == 'run':
         pipeline_file_path = sys.argv[2]
@@ -14,7 +14,7 @@ def get_pipeline_path():
             sys.exit(f'{pipeline_file_path} in wrong file type (has to be a .csv file)')
         return pipeline_file_path
     if option == 'make':
-        pipeline_file_path = sys.argv[2].replace(' ', '_') if sys.argv[2].endswith('.csv') else f'{sys.argv[2]}.csv'
+        pipeline_file_path = sys.argv[2] if sys.argv[2].endswith('.csv') else f'{sys.argv[2]}.csv'
         make_pipeline(pipeline_file_path)
         return pipeline_file_path
 
